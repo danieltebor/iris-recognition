6a99b5d458bce1ea96a036d2cad8113b17a2ab8d
Daniel Tebor <danieltebor@gmail.com>
2025-04-25 15:41:13 -0400

    Switch to AdamP optimizer and add warm restarts to training

--- scripts/triplet_loss_open_world.py
+++ scripts/triplet_loss_open_world.py
@@ -7,7 +7,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from torch.optim import AdamW
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
+from torch_optimizer import AdamP
 from torch.nn import TripletMarginLoss
 from torch.amp import autocast, GradScaler
 from torchvision import transforms
@@ -34,7 +35,7 @@ def train_model(config: dict) -> str:
     print('Loading data')
     transform = transforms.Compose([
         ModelFactory.get_resize_transform(config['model_name']),
-        transforms.RandomAffine(degrees=15, translate=(0.1, 0.1), scale=(0.9, 1.1)),
+        transforms.RandomAffine(degrees=15, translate=(0.05, 0.05), scale=(0.9, 1.05)),
         transforms.ToTensor(),
         ModelFactory.get_normalization_transform(config['model_name']),
     ])
@@ -51,9 +52,15 @@ def train_model(config: dict) -> str:
         config=config,
         is_triplet=True,
     )
+    val_eer_loader = _get_dataloader(
+        data_dir=config['data']['test_dir'],
+        transform=transform,
+        config=config,
+        is_triplet=False,
+    )
     
     print('Training model')
-    weights, epoch_metadata = _fit_model(model, device, train_loader, val_loader, config)
+    weights, epoch_metadata = _fit_model(model, device, train_loader, val_loader, val_eer_loader, config)
     
     current_datetime = datetime.now().strftime('%Y%m%d%H%M%S')
     model_dir = os.path.join(OUT_DIR, 'triplet-loss-open-world', config['experiment_name'], current_datetime)
@@ -63,7 +70,7 @@ def train_model(config: dict) -> str:
     save_file(weights, os.path.join(model_dir, f'{config['model_name']}.safetensors'))
     
     print('Saving metadata')
-    best_epoch = min(epoch_metadata, key=lambda x: x['val_loss'])['epoch']
+    best_epoch = min(epoch_metadata, key=lambda x: x['val_eer'])['epoch']
     train_images = list(set(train_loader.dataset.image_filenames.tolist()))
     val_images = list(set(val_loader.dataset.image_filenames.tolist()))
     metadata = {
@@ -71,8 +78,8 @@ def train_model(config: dict) -> str:
         'best_epoch': best_epoch,
         'epochs': epoch_metadata,
         'num_train_images': len(train_loader.dataset),
-        'train_images': train_images,
         'num_val_images': len(val_loader.dataset),
+        'train_images': train_images,
         'val_images': val_images,
     }
     with open(os.path.join(model_dir, 'meta', 'train.json'), 'w') as f:
@@ -80,14 +87,28 @@ def train_model(config: dict) -> str:
     
     return model_dir
 
-def _fit_model(model: nn.Module, device: str, train_loader: DataLoader, val_loader: DataLoader, config: dict) -> tuple[dict, list[dict]]:
+def _fit_model(
+    model: nn.Module,
+    device: str,
+    train_loader: DataLoader,
+    val_loader: DataLoader,
+    val_eer_loader: DataLoader,
+    config: dict
+) -> tuple[dict, list[dict]]:
     scaler = GradScaler()
-    optimizer = AdamW(model.parameters(), lr=config['learning_rate'], weight_decay=config['weight_decay'])
+    optimizer = AdamP(model.parameters(), lr=config['learning_rate'], weight_decay=config['weight_decay'])
     criterion = TripletMarginLoss(margin=config['margin'], p=2)
+    scheduler = CosineAnnealingWarmRestarts(
+        optimizer,
+        T_0=config['num_epochs'] // 10,
+        T_mult=2,
+        eta_min=config['learning_rate'] / 100,
+    )
     
     epoch_metadata = []
-    best_val_loss = float('inf')
+    best_val_eer = float('inf')
     best_weights = copy.deepcopy(model.state_dict())
+    num_epochs_since_best = 0
     
     for epoch in range(config['num_epochs']):
         total_train_loss = 0
@@ -131,20 +152,33 @@ def _fit_model(model: nn.Module, device: str, train_loader: DataLoader, val_load
                 loss = loss.item()
                 total_val_loss += loss
                 val_pbar.set_postfix({'loss': f'{loss:.4f}'})
+                
+            embeddings, labels = _extract_embeddings(model, device, val_eer_loader)
+            fpr, tpr, _ = compute_distance_roc(embeddings, labels)
+            val_eer = fpr[np.nanargmin(np.absolute((1 - tpr - fpr)))]
+        
+        scheduler.step()
         
         avg_train_loss = total_train_loss / len(train_loader)
         avg_val_loss = total_val_loss / len(val_loader)
-        print(f'Epoch, {epoch+1}/{config["num_epochs"]}, Train Loss: {avg_train_loss:.6f}, Val Loss: {avg_val_loss:.6f}')
-        
-        if avg_val_loss < best_val_loss:
-            best_val_loss = avg_val_loss
-            best_weights = copy.deepcopy(model.state_dict())
+        print(f'Epoch, {epoch+1}/{config["num_epochs"]}, Train Loss: {avg_train_loss:.6f}, Val Loss: {avg_val_loss:.6f}, Val EER: {val_eer:.6f}')
         
         epoch_metadata.append({
             'epoch': epoch+1,
             'train_loss': avg_train_loss,
             'val_loss': avg_val_loss,
+            'val_eer': val_eer,
         })
+        
+        if val_eer < best_val_eer:
+            best_val_eer = val_eer
+            best_weights = copy.deepcopy(model.state_dict())
+            num_epochs_since_best = 0
+        else:
+            num_epochs_since_best += 1
+            if num_epochs_since_best >= config['early_stopping_tolerance']:
+                print(f'No improvement in {num_epochs_since_best} epochs, stopping training')
+                break
         
     return best_weights, epoch_metadata
     
@@ -168,10 +202,17 @@ def eval_model(config, model_dir: str = None):
     _eval_angles(model, device, transform, model_dir, False, config)
     print('Computing angle-vs-frontal metrics')
     _eval_angles(model, device, transform, model_dir, True, config)
-    print('Computing TSNE')
+    print('Computing t-SNE')
     _eval_tsne(model, device, transform, model_dir, config)
     
-def _eval_angles(model: nn.Module, device: str, transform: transforms.Compose, model_dir: str, compare_with_frontal: bool, config: dict):
+def _eval_angles(
+    model: nn.Module,
+    device: str,
+    transform: transforms.Compose,
+    model_dir: str,
+    compare_with_frontal: bool,
+    config: dict
+):
     out_dir = os.path.join(model_dir, 'figs', 'angle-vs-frontal' if compare_with_frontal else 'angle-vs-angle')
     os.makedirs(out_dir, exist_ok=True)
     
@@ -246,11 +287,17 @@ def _eval_angles(model: nn.Module, device: str, transform: transforms.Compose, m
         legend_title='Angles' if compare_with_frontal else 'Angle',
         filename='roc',
         include_auc=False,
-        x_lim=(0.0, 0.1),
-        y_lim=(0.9, 1.0),
+        x_lim=(0.0, 0.02),
+        y_lim=(0.98, 1.0),
     )
     
-def _eval_tsne(model: nn.Module, device: str, transform: transforms.Compose, model_dir: str, config: dict):
+def _eval_tsne(
+    model: nn.Module,
+    device: str,
+    transform: transforms.Compose,
+    model_dir: str,
+    config: dict
+):
     test_loader = _get_dataloader(
         data_dir=config['data']['test_dir'],
         transform=transform,
@@ -300,12 +347,14 @@ def _extract_embeddings(model: nn.Module, device: str, loader: DataLoader) -> np
         labels = torch.cat(labels, dim=0).numpy()
         return embeddings, labels
     
-def _get_dataloader(data_dir: str,
-                    transform: transforms.Compose,
-                    config: dict,
-                    is_triplet: bool,
-                    shuffle: bool = False,
-                    angles: list[str] = None) -> DataLoader:
+def _get_dataloader(
+    data_dir: str,
+    transform: transforms.Compose,
+    config: dict,
+    is_triplet: bool,
+    shuffle: bool = False,
+    angles: list[str] = None
+) -> DataLoader:
     if is_triplet:
         dataset = IrisTripletDataset(
             data_dir=data_dir,
